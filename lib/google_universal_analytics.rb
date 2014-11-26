61b457698bcf856b74e0b7e75324083d8c1de549
David Boctor <dboctor@lumoslabs.com>
2014-11-25 16:20:32 -0800

    Explicitly specifying Javascript

--- lib/google_universal_analytics.rb
+++ lib/google_universal_analytics.rb
@@ -9,20 +9,18 @@ module GoogleUniversalAnalytics
     unless options[:tracker]
       raise InvalidOptionsError.new("Missing tracker number")
     end
-
-    "<script>\n" +
-    "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n" +
-    "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n" +
-    "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n" +
-    "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n" +
-
-    "ga('create', '#{options[:tracker]}'#{options[:domain] ? ", { cookieDomain: '#{options[:domain]}' }" : nil});\n" +
-
-    "#{custom_vars_jscript(options[:custom_vars])}\n" +
-    "#{track_events_jscript(options[:events])}\n" +
-    "ga('send', 'pageview');\n" +
-
-    "</script>\n"
+<<-JAVASCRIPT
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', '#{options[:tracker]}'#{options[:domain] ? ", { cookieDomain: '#{options[:domain]}' }" : nil});
+#{custom_vars_jscript(options[:custom_vars])}
+#{track_events_jscript(options[:events])}
+ga('send', 'pageview');
+</script>
+JAVASCRIPT
   end
 
   def custom_vars_jscript(custom_vars = [])
